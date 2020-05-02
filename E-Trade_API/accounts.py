935d85bdba874983064aea53089900bb26bb8b67
charley-padron <33139667+charley-padron@users.noreply.github.com>
2020-05-02 18:27:31 -0400

    Update accounts.py

--- E-Trade_API/accounts.py
+++ E-Trade_API/accounts.py
@@ -25,64 +25,64 @@ config.read('config.ini')
 
 # Read config file
 # login and authenticate
-class account:
-    def oauth(self):
-        """Allows user authorization for the sample application with OAuth 1"""
-        etrade = OAuth1Service(
-            name="etrade",
-            consumer_key=config["DEFAULT"]["CONSUMER_KEY"],
-            consumer_secret=config["DEFAULT"]["CONSUMER_SECRET"],
-            request_token_url="https://api.etrade.com/oauth/request_token",
-            access_token_url="https://api.etrade.com/oauth/access_token",
-            authorize_url="https://us.etrade.com/e/t/etws/authorize?key={}&token={}",
-            base_url="https://api.etrade.com")
-        
-        base_url = config["DEFAULT"]["PROD_BASE_URL"]
-        
-         # Step 1: Get OAuth 1 request token and secret
-        request_token, request_token_secret = etrade.get_request_token(
-            params={"oauth_callback": "oob", "format": "json"})
+
+def oauth():
+    """Allows user authorization for the sample application with OAuth 1"""
+    etrade = OAuth1Service(
+        name="etrade",
+        consumer_key=config["DEFAULT"]["CONSUMER_KEY"],
+        consumer_secret=config["DEFAULT"]["CONSUMER_SECRET"],
+        request_token_url="https://api.etrade.com/oauth/request_token",
+        access_token_url="https://api.etrade.com/oauth/access_token",
+        authorize_url="https://us.etrade.com/e/t/etws/authorize?key={}&token={}",
+        base_url="https://api.etrade.com")
     
-        # Step 2: Go through the authentication flow. Login to E*TRADE.
-        # After you login, the page will provide a text code to enter.
-        authorize_url = etrade.authorize_url.format(etrade.consumer_key, request_token)
-        
-        #############################################
-        # Automate the login process
-        # Version 17, now 18
-        # driver = webdriver.Edge(executable_path='C:/Users/charl/Downloads/edgedriver_win64/MicrosoftWebDriver.exe')
-        driver = webdriver.Edge()
-        
-        driver.get(authorize_url)
-        
-        logon = driver.find_element_by_id("logon_button")
-        logon.click()
-        
-        # not working
-       # driver.implicitly_wait(5)
-         
-        accept = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//form[@name='CustInfo']/input[3]")))
-         
-        #accept = driver.find_element_by_xpath("//form[@name='CustInfo']/input[3]")
-        accept.click()
-        
-        #code = driver.find_element_by_xpath("//div[@style='text-align:center']/input[1]")
-        code = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@style='text-align:center']/input[1]")))
-        code = code.get_attribute('value')
-        
-        driver.close()
-            
-        text_code = code
+    base_url = config["DEFAULT"]["PROD_BASE_URL"]
     
-        # Step 3: Exchange the authorized request token for an authenticated OAuth 1 session
-        session = etrade.get_auth_session(request_token,
-                                      request_token_secret,
-                                      params={"oauth_verifier": text_code})
+     # Step 1: Get OAuth 1 request token and secret
+    request_token, request_token_secret = etrade.get_request_token(
+        params={"oauth_callback": "oob", "format": "json"})
+
+    # Step 2: Go through the authentication flow. Login to E*TRADE.
+    # After you login, the page will provide a text code to enter.
+    authorize_url = etrade.authorize_url.format(etrade.consumer_key, request_token)
+    
+    #############################################
+    # Automate the login process
+    # Version 17, now 18
+    # driver = webdriver.Edge(executable_path='C:/Users/charl/Downloads/edgedriver_win64/MicrosoftWebDriver.exe')
+    driver = webdriver.Edge()
+    
+    driver.get(authorize_url)
+    
+    logon = driver.find_element_by_id("logon_button")
+    logon.click()
+    
+    # not working
+   # driver.implicitly_wait(5)
+     
+    accept = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//form[@name='CustInfo']/input[3]")))
+     
+    #accept = driver.find_element_by_xpath("//form[@name='CustInfo']/input[3]")
+    accept.click()
+    
+    #code = driver.find_element_by_xpath("//div[@style='text-align:center']/input[1]")
+    code = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@style='text-align:center']/input[1]")))
+    code = code.get_attribute('value')
+    
+    driver.close()
         
-        #automateTrades(session, base_url)
-        #print(session, base_url)
+    text_code = code
+
+    # Step 3: Exchange the authorized request token for an authenticated OAuth 1 session
+    session = etrade.get_auth_session(request_token,
+                                  request_token_secret,
+                                  params={"oauth_verifier": text_code})
+    
+    #automateTrades(session, base_url)
+    #print(session, base_url)
     
 if __name__ == "__main__":
-    account.oauth()
+    oauth()
